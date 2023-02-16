53b5b5a13dafa05dbeb9d3302b1fd906a751987f
Emiliano Parizzi <emiliano.parizzi@clevertech.biz>
2023-02-16 22:48:52 +0100

    initial commit

---
+++ src/modules/user/user.repository.ts
@@ -0,0 +1,12 @@
+import { UserRepository as MedusaUserRepository } from '@medusajs/medusa/dist/repositories/user';
+import { Repository as MedusaRepository } from 'medusa-extender';
+import { EntityRepository } from 'typeorm';
+import { User } from './user.entity';
+
+@MedusaRepository({ override: MedusaUserRepository })
+@EntityRepository(User)
+export default class UserRepository extends MedusaUserRepository {
+	test(): User {
+		return {} as any;
+	}
+}
