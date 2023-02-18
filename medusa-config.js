a2f5921448ebf195bfc61cf7e55017da65b1f0e8
Emiliano Parizzi <eparizzi@gmail.com>
2023-02-18 08:34:58 +0000

    Add Auth0 plugin

--- medusa-config.js
+++ medusa-config.js
@@ -20,8 +20,56 @@ const REDIS_URL = process.env.REDIS_URL || "";
 const PORT = process.env.PORT || 3000;
 const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000";
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:3000";
+const BACKEND_URL = "https://i4hs7o-3000.preview.csb.app";
 
-const plugins = [`medusa-fulfillment-manual`, `medusa-payment-manual`];
+const Auth0ClientId = process.env.AUTH0_CLIENT_ID || "";
+const Auth0ClientSecret = process.env.AUTH0_CLIENT_SECRET || "";
+const Auth0Domain = process.env.AUTH0_DOMAIN || "";
+
+const plugins = [
+  `medusa-fulfillment-manual`,
+  `medusa-payment-manual`,
+  {
+    resolve: "medusa-plugin-auth",
+    options: {
+      auth0: {
+        clientID: Auth0ClientId,
+        clientSecret: Auth0ClientSecret,
+        auth0Domain: Auth0Domain,
+
+        admin: {
+          callbackUrl: `${BACKEND_URL}/admin/auth/auth0/cb`,
+          failureRedirect: `https://www.google.com/`,
+
+          // The success redirect can be overriden from the client by adding a query param `?redirectTo=your_url`
+          // This query param will have the priority over this configuration
+          successRedirect: `https://www.google.com/`,
+
+          // authPath: '/admin/auth/auth0',
+          // authCallbackPath: '/admin/auth/auth0/cb',
+          // expiresIn: 24 * 60 * 60 * 1000,
+          // verifyCallback: (container, req, accessToken, refreshToken, extraParams, profile) => {
+          // implement your custom verify callback here if you need it
+          //    // console.log(container, req, accessToken, refreshToken, extraParams, profile)
+          // }
+        },
+
+        store: {
+          callbackUrl: `${BACKEND_URL}/store/auth/auth0/cb`,
+          failureRedirect: `https://www.google.com/`,
+
+          // The success redirect can be overriden from the client by adding a query param `?redirectTo=your_url`
+          // This query param will have the priority over this configuration
+          successRedirect: `https://www.google.com/`,
+
+          // authPath: '/store/auth/auth0',
+          // authCallbackPath: '/store/auth/auth0/cb',
+          // expiresIn: 24 * 60 * 60 * 1000,
+        },
+      },
+    },
+  },
+];
 
 module.exports = {
   serverConfig: {
@@ -33,7 +81,7 @@ module.exports = {
 
     database_type: "postgres",
     database_url: DATABASE_URL,
-    redis_url: REDIS_URL,
+    //redis_url: REDIS_URL,
 
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
